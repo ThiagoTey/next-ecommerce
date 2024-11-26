16953130de81beab498fcc2ad579185d199d36c0
ThiagoTey <teyextreme@outlook.com>
2024-11-26 15:13:18 -0300

    Product Image

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,6 +1,5 @@
 import { ProductType } from "@/types/ProductType"
-import Image from "next/image"
-
+import ProductImage from "./ProductImage"
 type Props = {
     product: ProductType
 }
@@ -9,7 +8,7 @@ const Product = ({ product }: Props) => {
     return (
         <div className="flex flex-col shadow-lg h-96 bg-slate-800 overflow-hidden p-5 text-gray-300">
             <div className="relative max-h-72 flex-1">
-                {/* <Image alt={product.title} src={product.image} width={100} height={100}/> */}
+                <ProductImage product={product} fill/>
             </div>
             <div className="flex justify-between font-bold my-3">
                 {product.title}
--- src/app/components/ProductImage.tsx
+++ src/app/components/ProductImage.tsx
@@ -1,19 +1,41 @@
 'use client'
 
+import { useState } from "react"
 import Image from "next/image";
 
 import { ProductType } from "@/types/ProductType"
 
 type Props = {
-    product: ProductType;
-    fill?: boolean; 
+  product: ProductType;
+  fill?: boolean;
 }
 
-const ProductImage = ({product, fill}: Props) => {
-  return  fill ?(
-    <Image src={product.image} fill alt={product.title} />
+const ProductImage = ({ product, fill }: Props) => {
+  const [loading, setLoading] = useState(true)
+
+  return fill ? (
+    <Image 
+      src={product.image} 
+      fill 
+      alt={product.title} 
+      className={`object-contain 
+      ${loading ? 'scale-110 blur-3xl grayscale'
+      : 'scale-100 blur-0 grayscale-0'
+      }`} 
+      onLoad={() => setLoading(false)}
+    />
   ) : (
-    <Image src={product.image}/>
+    <Image 
+      src={product.image} 
+      width={400}
+      height={700} 
+      alt={product.title} 
+      className={`object-contain 
+      ${loading ? 'scale-110 blur-3xl grayscale'
+      : 'scale-100 blur-0 grayscale-0'
+      }`} 
+      onLoadingComplete={() => setLoading(false)}
+  />
   )
 }
 
